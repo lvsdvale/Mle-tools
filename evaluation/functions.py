c166656dd8f09c54013d85214d18bd1706bf45a5
lvsdvale <vale@alunos.utfpr.edu.br>
2023-03-01 13:30:50 -0300

    feature: evaluation functions for regressor and classifiers

--- evaluation/functions.py
+++ evaluation/functions.py
@@ -1,6 +1,6 @@
 from imports import *
 
-def r2_score_evaluate_function(**kwargs):
+def r2_score_evaluation_function(**kwargs):
     data: pd.DataFrame = kwargs.get('data')
     target: str = kwargs.get('target')
     params = kwargs.get('params', None)
@@ -30,7 +30,7 @@ def r2_score_evaluate_function(**kwargs):
     return r2_score(y_validation, y_pred)
 
 
-def mean_absolute_percente_error_optimization_function(**kwargs):
+def mean_absolute_percente_error_evaluation_function(**kwargs):
     data: pd.DataFrame = kwargs.get('data')
     target: str = kwargs.get('target')
     params = kwargs.get('params', None)
@@ -59,7 +59,7 @@ def mean_absolute_percente_error_optimization_function(**kwargs):
     y_pred = model.predict(x_validation)
     return mean_absolute_percentage_error(y_validation, y_pred)
 
-def mean_absolute_error_optimization_function(**kwargs):
+def mean_absolute_error_evaluation_function(**kwargs):
     data: pd.DataFrame = kwargs.get('data')
     target: str = kwargs.get('target')
     params = kwargs.get('params', None)
@@ -87,3 +87,123 @@ def mean_absolute_error_optimization_function(**kwargs):
     y_validation = validation_data[target]
     y_pred = model.predict(x_validation)
     return mean_absolute_error(y_validation, y_pred)
+
+def recall_evaluation_function(**kwargs):
+    data: pd.DataFrame = kwargs.get('data')
+    target: str = kwargs.get('target')
+    params = kwargs.get('params', None)
+    model = kwargs.get('model')
+    is_validation = kwargs.get('is_validation', False)
+    x = data.drop(target, axis=1)
+    y = data[target]
+    if is_validation is False:
+        n_splits = kwargs.get('n_splits')
+        kfold = KFold(n_splits=n_splits)
+        if params is not None:
+            model.set_params(**params)
+        kfold.get_n_splits(x)
+        scores = list()
+        for train_index, test_index in kfold.split(x):
+            x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+            scores.append(recall_score(y_test, y_pred))
+        return np.mean(scores)
+    validation_data: pd.DataFrame = kwargs.get('validation_data')
+    model.fit(x, y)
+    x_validation = validation_data.drop(target, axis=1)
+    y_validation = validation_data[target]
+    y_pred = model.predict(x_validation)
+    return recall_score(y_validation, y_pred)
+
+
+def precision_evaluation_function(**kwargs):
+    data: pd.DataFrame = kwargs.get('data')
+    target: str = kwargs.get('target')
+    params = kwargs.get('params', None)
+    model = kwargs.get('model')
+    is_validation = kwargs.get('is_validation', False)
+    x = data.drop(target, axis=1)
+    y = data[target]
+    if is_validation is False:
+        n_splits = kwargs.get('n_splits')
+        kfold = KFold(n_splits=n_splits)
+        if params is not None:
+            model.set_params(**params)
+        kfold.get_n_splits(x)
+        scores = list()
+        for train_index, test_index in kfold.split(x):
+            x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+            scores.append(precision_score(y_test, y_pred))
+        return np.mean(scores)
+    validation_data: pd.DataFrame = kwargs.get('validation_data')
+    model.fit(x, y)
+    x_validation = validation_data.drop(target, axis=1)
+    y_validation = validation_data[target]
+    y_pred = model.predict(x_validation)
+    return precision_score(y_validation, y_pred)
+
+
+def f1_score_evaluation_function(**kwargs):
+    data: pd.DataFrame = kwargs.get('data')
+    target: str = kwargs.get('target')
+    params = kwargs.get('params', None)
+    model = kwargs.get('model')
+    is_validation = kwargs.get('is_validation', False)
+    x = data.drop(target, axis=1)
+    y = data[target]
+    if is_validation is False:
+        n_splits = kwargs.get('n_splits')
+        kfold = KFold(n_splits=n_splits)
+        if params is not None:
+            model.set_params(**params)
+        kfold.get_n_splits(x)
+        scores = list()
+        for train_index, test_index in kfold.split(x):
+            x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+            scores.append(f1_score(y_test, y_pred))
+        return np.mean(scores)
+    validation_data: pd.DataFrame = kwargs.get('validation_data')
+    model.fit(x, y)
+    x_validation = validation_data.drop(target, axis=1)
+    y_validation = validation_data[target]
+    y_pred = model.predict(x_validation)
+    return f1_score(y_validation, y_pred)
+
+def accuracy_evaluation_function(**kwargs):
+    data: pd.DataFrame = kwargs.get('data')
+    target: str = kwargs.get('target')
+    params = kwargs.get('params', None)
+    model = kwargs.get('model')
+    is_validation = kwargs.get('is_validation', False)
+    x = data.drop(target, axis=1)
+    y = data[target]
+    if is_validation is False:
+        n_splits = kwargs.get('n_splits')
+        kfold = KFold(n_splits=n_splits)
+        if params is not None:
+            model.set_params(**params)
+        kfold.get_n_splits(x)
+        scores = list()
+        for train_index, test_index in kfold.split(x):
+            x_train, x_test = x.iloc[train_index], x.iloc[test_index]
+            y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+            model.fit(x_train, y_train)
+            y_pred = model.predict(x_test)
+            scores.append(accuracy_score(y_test, y_pred))
+        return np.mean(scores)
+    validation_data: pd.DataFrame = kwargs.get('validation_data')
+    model.fit(x, y)
+    x_validation = validation_data.drop(target, axis=1)
+    y_validation = validation_data[target]
+    y_pred = model.predict(x_validation)
+    return accuracy_score(y_validation, y_pred)
+
+
--- imports.py
+++ imports.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
+from matplotlib.ticker import PercentFormatter
 import optuna
 from sklearn.metrics import mean_absolute_error, r2_score, mean_absolute_percentage_error, plot_confusion_matrix, accuracy_score, recall_score, precision_score, f1_score
 from sklearn.model_selection import train_test_split, cross_val_score , KFold 
