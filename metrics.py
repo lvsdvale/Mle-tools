53f06d064eba80550cb4e4f367b648ecd2e962b4
lvsdvale <vale@alunos.utfpr.edu.br>
2023-03-15 16:47:44 -0300

    refactor: metrics

--- metrics.py
+++ metrics.py
@@ -1,6 +1,36 @@
 from imports import *
 
-def metrics(model, real_target, predicted_target) -> pd.DataFrame:
+def classification_metrics(model, real_target, predicted_target) -> pd.DataFrame:
+    """
+    An metric function using f1, recall, accuracy, precision, true negatives, true positive, false negative e false positive values.
+
+    param model: sklean model
+        'model param is the machine learning algorithm that was used for our fitting
+    :param real_target: np.array
+        the real_target param is our target column of real data
+    param predicted_target: np.array
+        the predicted_target param is our prediction  of target column 
+    return
+        a dataframe with all metrics values above
+    """
+    metrics_dict = dict()
+    metrics_dict['Model'] = [model,]
+    metrics_dict['F1'] = [f1_score(real_target, predicted_target),]
+    metrics_dict['Accuracy'] = [accuracy_score(real_target, predicted_target),]
+    metrics_dict['Precision'] = [precision_score(real_target, predicted_target),]
+    metrics_dict['Recall'] = [recall_score(real_target, predicted_target),]
+    true_negative, false_positive, false_negative, true_positive = confusion_matrix(real_target, predicted_target).ravel()
+    metrics_dict['True Positive'] = true_positive
+    metrics_dict['False Positive'] = false_positive
+    metrics_dict['True Negative'] = true_negative
+    metrics_dict['False Negative'] =false_negative
+    metrics_dataframe = pd.DataFrame.from_dict(metrics_dict)
+    return metrics_dataframe
+
+
+from imports import *
+
+def regression_metrics(model, real_target, predicted_target) -> pd.DataFrame:
     """
     An metric function using r2, mean absolute error, mean absolute percentage error, % Of Predictions With Error < 5, 10, 15 and 30 values.
     param model: sklean model
